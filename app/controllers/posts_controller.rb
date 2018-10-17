253af635deb166577200aee0a695dce981782253
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-17 22:21:18 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,13 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
       #BEFORE using a serializer:
-      render json: @post.to_json(only: [:title, :description, :id],
-                                include: [author: { only: [:name]}])
+      # render json: @post.to_json(only: [:title, :description, :id],
+      #                           include: [author: { only: [:name]}])
        # AFTER USING OUR SERIALIZER
-      # render json: @post, status: 200
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post, status: 200 }
+    end
   end
 
   def new
